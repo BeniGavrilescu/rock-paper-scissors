c40f2234de9274cde652b5eb0660485188112e7a
BeniGavrilescu <beniamin.gavrilescu2024@gmail.com>
2025-10-11 11:55:01 +0300

    Added a function of number of rounds and adding a condition system for chooses (userChoose vs computerChoose)

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let number = parseInt(prompt("Please insert the number of round of the rock-paper-scissors: "));
+let number = parseInt(prompt("Please insert the number of round of the rock-paper-scissors: ")), userPoints = 0, computerPoints = 0;
+if(number == 1)
+    console.log("You choosed to play one round.");
+else
+    console.log("You choosed to play", number, "rounds.");
+const chooses = ["rock", "paper", "scissors"];
+console.log("Before to start, you will make your choice by typing a number. 0 is for rock, 1 is for paper, 2 is for scissors");
+for(let i = 1; i <= bumber; i++) {
+    let userChoose = parseInt(prompt("Type 1 (rock), 2 (paper), 3 (scissors): "));
+    let computerChoise = Math.floor(Math.random() * 3);
+    if(userChoose == 1) {
+
+    }
+    else if(userChoose == 2) {
+        
+    }
+    else if(userChoose == 3) {
+
+    }
+    else {
+
+    }
+}
