d42f9a59766c91373b628f68eed5e3e9a784392a
BeniGavrilescu <beniamin.gavrilescu2024@gmail.com>
2025-10-11 12:07:17 +0300

    Completed the condition system

--- script.js
+++ script.js
@@ -5,19 +5,53 @@ else
     console.log("You choosed to play", number, "rounds.");
 const chooses = ["rock", "paper", "scissors"];
 console.log("Before to start, you will make your choice by typing a number. 0 is for rock, 1 is for paper, 2 is for scissors");
-for(let i = 1; i <= bumber; i++) {
+for(let i = 1; i <= number; i++) {
+    console.log(" ");
+    console.log("Round", i);
     let userChoose = parseInt(prompt("Type 1 (rock), 2 (paper), 3 (scissors): "));
     let computerChoise = Math.floor(Math.random() * 3);
     if(userChoose == 1) {
-
+        if(computerChoise == 1)
+            console.log("It's a tie");
+        else if(computerChoise == 2) {
+            console.log("You win a point.");
+            userPoints++;
+        }
+        else {
+            console.log("The computer win a point.");
+            computerPoints++;
+        }
     }
     else if(userChoose == 2) {
-        
+        if(computerChoise == 1) {
+            console.log("The computer win a point.");
+            computerPoints++;
+        }
+        else if(computerChoise == 2) {
+            console.log("It's a tie");
+        }
+        else {
+            console.log("You win a point.");
+            userPoints++;
+        }
     }
     else if(userChoose == 3) {
-
+        if(computerChoise == 1) {
+            console.log("The computer win a point.");
+            computerPoints++;
+        }
+        else if(computerChoise == 2) {
+            console.log("You win a point.")
+            userPoints++;
+        }
+        else {
+            console.log("It's a tie");
+        }
+        console.log("User points:", userPoints);
+        console.log("Computer points: ", computerPoints);
     }
     else {
-
+        console.log("Wrong choose");
+        i--;
     }
 }
