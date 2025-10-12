a4b8b57be0c156ec5184c9458720748f5483d344
BeniGavrilescu <beniamin.gavrilescu2024@gmail.com>
2025-10-12 15:07:22 +0300

    v2 (display with popup)

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
-let number = parseInt(prompt("Please insert the number of round of the rock-paper-scissors: ")), userPoints = 0, computerPoints = 0;
+let number = parseInt(prompt("Please insert the number of round of the rock-paper-scissors: "));
+let userPoints = 0;
+let computerPoints = 0;
+
 if(number == 1)
     console.log("You choosed to play one round.");
 else
     console.log("You choosed to play", number, "rounds.");
 const chooses = ["rock", "paper", "scissors"];
 console.log("Before to start, you will make your choice by typing a number. 0 is for rock, 1 is for paper, 2 is for scissors");
+
 for(let i = 1; i <= number; i++) {
-    console.log(" ");
-    console.log("Round", i);
-    let userChoose = parseInt(prompt("Type 1 (rock), 2 (paper), 3 (scissors): "));
+    alert("Round " + i + ".");
+    let userChoose = parseInt(prompt("Type 0 (rock), 1 (paper), 2 (scissors): ")), touchedTheButton = false, computerWin = false, userWin = false;
     let computerChoise = Math.floor(Math.random() * 3);
-    if(userChoose == 1) {
-        if(computerChoise == 1)
-            console.log("It's a tie");
-        else if(computerChoise == 2) {
-            console.log("You win a point.");
-            userPoints++;
-        }
-        else {
-            console.log("The computer win a point.");
-            computerPoints++;
-        }
-    }
-    else if(userChoose == 2) {
+
+    if(userChoose == 0) { // user choose rock
         if(computerChoise == 1) {
-            console.log("The computer win a point.");
+            computerWin = true;
             computerPoints++;
         }
         else if(computerChoise == 2) {
-            console.log("It's a tie");
-        }
-        else {
-            console.log("You win a point.");
+            userWin = true;
             userPoints++;
         }
     }
-    else if(userChoose == 3) {
-        if(computerChoise == 1) {
-            console.log("The computer win a point.");
-            computerPoints++;
+    else if(userChoose == 1) { // user choose paper
+        if(computerChoise == 0) {
+            userWin = true;
+            userPoints++;
         }
         else if(computerChoise == 2) {
-            console.log("You win a point.")
-            userPoints++;
-        }
-        else {
-            console.log("It's a tie");
+            computerWin = true;
+            computerPoints++;
         }
     }
-    else {
-        console.log("Wrong choose");
+    else if(userChoose == 2) { // user choose scissors
+        if(computerChoise == 0) {
+            computerWin = true;
+            computerPoints++;
+        } 
+        else if(computerChoise == 1) {
+            userWin = true;
+            userPoints++;
+        }
+    }
+    else { // if user make wrong choose
+        alert("Wrong choose.")
+        touchedTheButton = true;
         i--;
     }
-    console.log("User points:", userPoints);
-    console.log("Computer points: ", computerPoints);
+
+
+    if(touchedTheButton == false) 
+        alert("You choosed the " + chooses[userChoose] + ".\nThe computer choosed " + chooses[computerChoise] + ".");
+    if(computerWin == true) 
+        alert("The computer win a point. \n Computer points: " + computerPoints + "\n User points: " + userPoints);
+    else if(userWin == true)
+        alert("You win a point. \n Computer points: " + computerPoints + "\n User points: " + userPoints);
+    else if(touchedTheButton == false)
+        alert("It's a tie. \n Computer points: " + computerPoints + "\n User points: " + userPoints);
 }
+
 if(computerPoints > userPoints)
-    console.log("The computer win");
+    alert("The computer win");
 else if(userPoints > computerPoints)
-    console.log("You win");
+    alert("You win");
 else
-    console.log("Final: It's a tie");
+    alert("Final: It's a tie");
